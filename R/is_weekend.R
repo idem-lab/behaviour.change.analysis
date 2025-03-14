2b759bb0bb970dee4274f3ef363779d0386b9609
geryan <geryan@users.noreply.github.com>
2025-03-14 17:00:08 +1100

    NO

--- R/is_weekend.R
+++
@@ -1,3 +0,0 @@
-is_weekend <- function(date) {
-  lubridate::wday(date, label = TRUE) %in% c("Sat", "Sun")
-}
--- R/yesno_to_logical.R
+++
@@ -1,7 +0,0 @@
-yesno_to_logical <- function(x) {
-  dplyr::case_when(
-    x == "Yes" ~ TRUE,
-    x == "No" ~ FALSE,
-    TRUE ~ NA
-  )
-}
