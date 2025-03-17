ca3581caf7165d033f815bed5bf9096b7e2c07ad
geryan <geryan@users.noreply.github.com>
2025-03-17 16:08:48 +1100

    export `gaussian_smooth`

--- R/gaussian_smooth.R
+++ R/gaussian_smooth.R
@@ -1,8 +1,20 @@
+#' @title Gaussian smooth for data
+#'
+#' @param values `numeric`
+#' @param sd `numeric`
+#' @param ...
+#'
+#' @returns
+#' @export
+#'
+#' @examples
 gaussian_smooth <- function (values, sd = 1, ...) {
+
   id <- seq_along(values)
   middle <- round(mean(id))
   diff <- id - middle
   weights <- exp(-0.5 * (diff / sd) ^ 2)
   weights <- weights / sum(weights)
   weighted.mean(values, weights, ...)
+
 }
